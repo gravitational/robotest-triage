8063f696aea68d9c0c5afe0b3fef0ce71f61b414
Walt Della <walt@gravitational.com>
2020-04-09 16:38:24 -0700

    Allow unspecified start & end jids.
    I typically want to fetch the latest job data from Jenkins.  It is
    convenient to be able to do this without tweaking config.json.

--- fetch.py
+++ fetch.py
@@ -14,6 +14,15 @@ def fetch(url, auth, filepath):
                     f.write(chunk)
 
 
+def get_latest_job(job_url, auth):
+    api_url = job_url + "/api/json"
+    with requests.get(api_url, auth=auth) as r:
+        r.raise_for_status()
+        json = r.json()
+    latest = json["builds"][0]["number"]
+    return latest
+
+
 if __name__ == "__main__":
     with open("config.json", 'r') as cf:
         config = json.load(cf)
@@ -26,9 +35,17 @@ if __name__ == "__main__":
     datadir = DATA_DIR  # const for now, consider making configurable -- walt 2020-04
     server = config["url"]
     job = config["job"]
-    jid_start = config["jid_start"]
-    jid_end = config["jid_end"]
     job_url = server + "/job/" + job
+
+    jid_start = config.get("jid_start", 1)
+    if not jid_start > 0:
+        raise ValueError("jid_start (%i) must be > 0" % jid_start)
+    jid_end = config.get("jid_end", "latest")
+    if jid_end == "latest":
+        jid_end = get_latest_job(job_url, auth)
+    if not jid_start < jid_end:
+        raise ValueError("jid_start (%s) must be < jid_end (%s)" % (jid_start, jid_end))
+
     console_url_template = job_url + "/{jid}/consoleText"
 
     for jid in range(jid_start, jid_end + 1):
